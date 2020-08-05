1920066aa34dbb345fc3a2fb09a90992af6e4329
Ali Najafi <najafi1998ali@gmail.com>
2020-08-05 10:13:25 +0430

    BinarySearch is Done!

---
+++ Searching/BinarySearch.py
@@ -0,0 +1,23 @@
+class BinarySearch:
+    def __init__(self, array: list):
+        self.array = array
+
+    def search(self, value):
+        return self.search_value(0, len(self.array), value)
+
+    def search_value(self, low, high, value):
+        if low > high:
+            return None
+        mid = low + (high - low) // 2
+        if abs(self.array[mid] - value) < 1e-9:
+            return mid
+        elif self.array[mid] > value:
+            return self.search_value(low, mid - 1, value)
+        elif self.array[mid] < value:
+            return self.search_value(mid + 1, high, value)
+
+
+if __name__ == '__main__':
+    sorted_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    bs = BinarySearch(sorted_arr)
+    print(bs.search(5))
